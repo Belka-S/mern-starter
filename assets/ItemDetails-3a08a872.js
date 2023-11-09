4a8c361222f3bedabd727eed19362f5ac1791276
Belka-S <Belka-S@users.noreply.github.com>
2023-11-09 13:12:45 +0000

    deploy: 1f1af7569fa4f8a32ac0cdf07bc15dd799eaa660

--- assets/ItemDetails-3a08a872.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-3c47c22e.js";const t="_details_1fsak_1",e={details:t},i=()=>s.jsx("div",{className:e.details,children:"ItemDetails Page"});export{i as default};
---
+++ assets/Items-bd843ae4.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-19a20be3.js";const t="_items_1vw4k_1",e={items:t},i=()=>s.jsx("div",{className:e.items,children:"Items Page"});export{i as default};
